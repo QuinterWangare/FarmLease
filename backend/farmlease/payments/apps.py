93eb50cdfd79a9f2df573a2d96a16af352f1a436
Martin-Ndirangu <martinnjoroge3266@gmail.com>
2026-02-10 12:12:04 +0300

    authentication

--- backend/farmlease/payments/apps.py
+++ backend/farmlease/payments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-#name should match the name of the app folder
+#name should match the name of the app folder.
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
